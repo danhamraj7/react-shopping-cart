c9ada4e8fd006531377cefb572ecf807d7c7ae5d
danhamraj <danhamraj@gmail.com>
2019-12-06 20:19:15 -0500

    fix props error on navigation and products,product.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 //contexts
 import { ProductContext } from "./contexts/ProductContext";
-
+import { CartContext } from "./contexts/CartContext";
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
@@ -21,17 +21,15 @@ function App() {
   };
 
   return (
-	<div className="App">
-	{/* step3 wrapped components with provider */}
+    <div className="App">
+      {/* step3 wrapped components with provider */}
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-
-        {/* Routes */}
-        <Route
-		<Route exact path="/" component={Products} />
-        />
-
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={[cart]}>
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 //step 4 imported ProductContext and removed props
 // Components
 import Product from "./Product";
 import { ProductContext } from "../contexts/ProductContext";
 
-const Products = props => {
+const Products = () => {
   const { products, addItem } = useContext(ProductContext);
   return (
     <div className="products-container">
       {products.map(product => (
-        <Product key={product.id} product={product} addItem={props.addItem} />
+        <Product key={product.id} product={product} addItem={addItem} />
       ))}
     </div>
   );
